31b71af44f2cdaa2355f3e41e31e1e85c6c3f3a1
Donatas <donatas.sul@gmail.com>
2023-04-04 17:50:21 +0300

    Added navigation logic and tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Gallery from "./pages/Gallery";
+import Prices from "./pages/Prices";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/prices" element={<Prices />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,5 @@
+function Blog() {
+  return <h1>This is blog page</h1>;
+}
+
+export default Blog;
