267ba79f2b5f4ed6adcff4c6b5a212c38e58dc8b
Donatas <donatas.sul@gmail.com>
2023-04-05 19:40:53 +0300

    Added main navigation

---
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -0,0 +1,50 @@
+import { Link, Button, Toolbar, Box, AppBar } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const navItems = [
+  { name: 'Pagrindinis', route: '/' },
+  { name: 'Apie mane', route: '/about' },
+  { name: 'Galerija', route: '/gallery' },
+  { name: 'Paslaugos', route: '/' },
+  { name: 'Kainos', route: '/prices' },
+  { name: 'Blogas', route: '/blog' },
+];
+
+function MainNavigation() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav" color="transparent" sx={{ boxShadow: 'none' }}>
+        <Toolbar sx={{ justifyContent: 'center' }}>
+          <Box>
+            {navItems.map((item) => (
+              <Button
+                disableRipple
+                key={item.name}
+                sx={{
+                  pr: 4,
+                  '&:nth-child(3)': {
+                    pr: 28,
+                  },
+                  '&:hover': {
+                    backgroundColor: 'transparent',
+                  },
+                }}
+              >
+                <Link
+                  sx={{ fontSize: 11, color: '#f3e9e0', backgroundColor: 'transparent' }}
+                  component={RouterLink}
+                  to={item.route}
+                  underline="none"
+                >
+                  {item.name}
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default MainNavigation;
