eaf4d947cf3efd370e2081aa3aa9e30110e09028
Donatas Šulskis <77921485+Donatas-Sulskis@users.noreply.github.com>
2023-04-06 17:46:08 +0300

    Added main navigation and added prettier script (#5)
    * Added main navigation
    
    * change nav sequence
    
    ---------
    
    Co-authored-by: Vytautas Krapikas <vytautaskrapikas@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import Gallery from './pages/Gallery';
 import Prices from './pages/Prices';
 import Blog from './pages/Blog';
 import About from './pages/About';
+import { Box } from '@mui/material';
+import MainNavigation from './components/MainNavigation/MainNavigation';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/gallery" element={<Gallery />} />
-      <Route path="/prices" element={<Prices />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <Box component="main">
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/prices" element={<Prices />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Box>
   );
 }
 
