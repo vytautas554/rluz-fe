02eccb3a8aabad35c61913c86b438a3b4ec9589c
Donatas <donatas.sul@gmail.com>
2023-04-13 19:26:39 +0300

    feat/add base stepper

---
+++ src/components/ReviewSection/ReviewSection.tsx
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const reviews = [
+  {
+    person: 'Jonasssssssssss',
+    review:
+      'Three words: Best. Photographer. Ever. She is so talented and can work WONDERS with lighting. Every single picture she took we LOVE. Making it nearly impossible to pick our favorites. She is absolutely hilarious and tells you exactly what to do for each shot, and they all turn out so natural and perfect. Nothing is ever awkward when working with Jackie. She is beyond professional while at the same time making you feel like youve known her forever. I could go on and on about how absolutely amazing she is and how thankful we are that we found her! We cannot wait for her to capture our wedding, and continue to capture any milestones we have down the road. She has earned our business for life! Do yourself a favor and book with her, NOW. You wont be disappointed!',
+  },
+  {
+    person: 'Petras',
+    review:
+      'Jaclyn is nothing short of amazing!!! Jaclyn and my boyfriend Ty at the time we’re planning a surprise proposal during our photoshoot. She took the time to talk and plan out the whole shoot with Ty to make it the most perfect moment. She was amazing during the whole shoot and took beautiful photos that we love and will always be able to look back on! Meeting her felt like I knew her for years! I will definitely be using her for more photo shoots! I recommend her and always will!! Thanks Jaclyn for everything you did in making our engagement so special!!! xx',
+  },
+  {
+    person: 'Kazys',
+    review:
+      'I cannot recommend Jackie  enough! My husband and I are awkward in front of the camera, but she made us feel entirely comfortable. PLUS she photographed us with our two crazy pups for both the engagement shoot and on our wedding day and somehow managed to get amazing pictures even with them jumping around. Not only is her work incredible, shes so easy to get along with and we genuinely enjoyed spending time with her - so much so that we were sad to see her go when she was finished shooting on our wedding day. She made everything completely stress free and we made the BEST choice to book her as our photographer',
+  },
+];
+
+function ReviewSection() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = reviews.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundImage: 'url("//static.showit.co/1600/2KJC7dO0SjyKxVooWWzoJQ/shared/xxroses-textures-cremenoisebckgnd.png")',
+        py: 8,
+      }}
+    >
+      <Box sx={{ mx: 40, position: 'relative' }}>
+        <Box sx={{ height: 255, px: 35, width: '100%', textAlign: 'justify', fontFamily: 'Marcellus' }}>
+          {reviews[activeStep].review}
+        </Box>
+        <MobileStepper
+          sx={{ position: 'relative', background: 'none' }}
+          steps={0}
+          position="static"
+          nextButton={
+            <Button
+              disableRipple
+              sx={{
+                position: 'absolute',
+                left: '85%',
+                top: 5,
+                color: 'black',
+                '&:hover': {
+                  backgroundColor: 'transparent',
+                },
+                fontFamily: 'Marcellus',
+              }}
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
+              —Next Review
+            </Button>
+          }
+          backButton={
+            <Button
+              disableRipple
+              sx={{
+                position: 'absolute',
+                right: '85%',
+                top: -300,
+                color: 'black',
+                '&:hover': {
+                  backgroundColor: 'transparent',
+                },
+                fontFamily: 'Marcellus',
+              }}
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              —Previous Review
+            </Button>
+          }
+        ></MobileStepper>
+        <Box
+          sx={{
+            position: 'absolute',
+            background: 'none',
+            right: '50%',
+            top: '70%',
+            fontFamily: 'Marcellus',
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
+            <Typography sx={{ width: '1px', backgroundColor: 'black', height: 28 }}></Typography>
+            <Typography sx={{ fontWeight: 500, pt: 1, fontSize: 18, fontFamily: 'Marcellus' }}>
+              - {reviews[activeStep].person}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default ReviewSection;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,7 @@ import TransitionSection from '../components/TransitionSection/TransitionSection
 import transitionImage1 from '../images/transitions/DSC_3052-Edit-Edit-Edit.jpg';
 import transitionImage2 from '../images/transitions/DSC_3506-Edit.jpg';
 import GallerySection from '../components/GallerySection/GallerySection';
+import ReviewSection from '../components/ReviewSection/ReviewSection';
 
 function Main() {
   return (
@@ -20,6 +21,7 @@ function Main() {
         text="making your once in a lifetime day last forever
 "
       />
+      <ReviewSection />
     </Box>
   );
 }
