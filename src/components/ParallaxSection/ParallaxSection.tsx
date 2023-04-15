ecb6b28e8cbe7994d153cd890651f2774489e6b9
Donatas <donatas.sul@gmail.com>
2023-04-15 09:49:46 +0300

    feat/Add parallax section

---
+++ src/components/ParallaxSection/ParallaxSection.tsx
@@ -0,0 +1,40 @@
+import { Link, Box, Typography } from '@mui/material';
+import { ImageValues } from '../common/interfaces';
+import { Link as RouterLink } from 'react-router-dom';
+
+export function ParallaxSection({ url, description, text }: ImageValues) {
+  return (
+    <Box sx={{ height: 450 }}>
+      <Box
+        sx={{
+          backgroundImage: `url('${url}')`,
+          height: '100%',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+        title={description}
+      >
+        <Typography sx={{ fontSize: 26, fontFamily: 'Marcellus', textAlign: 'center', textTransform: 'uppercase', pt: 25 }}>
+          <Link
+            sx={{
+              color: 'white',
+              transition: 'color 0.4s',
+              '&:hover': {
+                color: 'black',
+              },
+            }}
+            component={RouterLink}
+            to={'/prices'}
+            underline="none"
+          >
+            {text}
+          </Link>
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default ParallaxSection;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,6 +6,8 @@ import transitionImage1 from '../images/transitions/DSC_3052-Edit-Edit-Edit.jpg'
 import transitionImage2 from '../images/transitions/DSC_3506-Edit.jpg';
 import GallerySection from '../components/GallerySection/GallerySection';
 import ReviewSection from '../components/ReviewSection/ReviewSection';
+import ParallaxSection from '../components/ParallaxSection/ParallaxSection';
+import ParallaxImage from '../images/transitions/Brigita ir Sigitas.3148-Edit.jpg';
 
 function Main() {
   return (
@@ -22,6 +24,7 @@ function Main() {
 "
       />
       <ReviewSection />
+      <ParallaxSection url={ParallaxImage} description="" text="Come to see what I offer" />
     </Box>
   );
 }
