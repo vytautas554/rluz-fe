487e784348facd40d4e921bbcf0a478ae9682844
vytautas554 <60849219+vytautas554@users.noreply.github.com>
2023-04-20 14:02:54 +0300

    feat/add-main-footer (#13)
    * add main footer
    
    * Pr fix
    
    * pr fix
    
    * pr fix
    
    * add transition

---
+++ src/components/MainFooter/MainFooter.tsx
@@ -0,0 +1,81 @@
+import { Typography, Link, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import Logo from '../../images/signature-and-logo/permatomas-baltas-auksas.png';
+import Img from '../../../src/images/Vestuvių fotosesija.jpg';
+import Img1 from '../../../src/images/Nėtumo fotosesija.jpg';
+import Img2 from '../../../src/images/Poros fotosesija.jpg';
+import Img3 from '../../../src/images/Justina ir Vilius.2701-Edit-3.jpg';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { FACEBOOK_LINK, INSTAGRAM_LINK } from '../utils/constant';
+
+const images = [{ src: Img }, { src: Img1 }, { src: Img2 }, { src: Img3 }, { src: Img }, { src: Img2 }];
+
+function MainFooter() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        flexDirection: 'column',
+        background: '#e0c5aa',
+        py: 4,
+      }}
+    >
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <img style={{ width: 200, height: 200 }} src={Logo} alt="logo" />
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box
+            sx={{ width: '30%', display: 'flex', justifyContent: 'center', borderTop: 1, borderBottom: 1, borderColor: 'white' }}
+          >
+            <Box sx={{ width: '10%', display: 'flex', justifyContent: 'space-between', my: 2 }}>
+              <Link component={RouterLink} to={FACEBOOK_LINK} underline="none" target="_blank">
+                <FacebookRoundedIcon sx={{ color: 'white' }} />
+              </Link>
+              <Link component={RouterLink} to={INSTAGRAM_LINK} underline="none" target="_blank">
+                <InstagramIcon sx={{ color: 'white' }} />
+              </Link>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Link component={RouterLink} to={INSTAGRAM_LINK} target="_blank" underline="none" sx={{ color: 'white' }}>
+          <Typography
+            variant="h5"
+            sx={{
+              pb: 1,
+              textAlign: 'center',
+              '&:hover': {
+                transition: 'all 0.4s ease-in-out',
+                color: '#c4a858',
+              },
+            }}
+          >
+            @r_luz_photography
+          </Typography>
+        </Link>
+      </Box>
+      <Box sx={{ display: 'flex', width: '100%', alignItems: 'center', flexDirection: 'column', py: 2 }}>
+        <Box>
+          <Link component={RouterLink} to={INSTAGRAM_LINK} target="_blank" underline="none">
+            {images.map((image, index) => (
+              <img
+                style={{ width: 160, height: 160, objectFit: 'cover', marginRight: '3px' }}
+                key={index}
+                src={image.src}
+                alt={`Image ${index}`}
+              />
+            ))}
+          </Link>
+        </Box>
+      </Box>
+      <Box sx={{ borderBottom: 1, borderColor: 'white', width: '40%' }}></Box>
+    </Box>
+  );
+}
+
+export default MainFooter;
